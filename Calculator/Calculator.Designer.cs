03847d27902e44696f37554a4ef3a5b42ff2f366
Zesty Fernandez <superdeadman99@gmail.com>
2016-04-04 21:17:59 -0400

    Initial commit

---
+++ Calculator/Calculator.Designer.cs
@@ -0,0 +1,309 @@
+﻿using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    partial class FrmCalc
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.button12 = new System.Windows.Forms.Button();
+            this.button13 = new System.Windows.Forms.Button();
+            this.button14 = new System.Windows.Forms.Button();
+            this.button15 = new System.Windows.Forms.Button();
+            this.exp = new System.Windows.Forms.Button();
+            this.sqrt = new System.Windows.Forms.Button();
+            this.button18 = new System.Windows.Forms.Button();
+            this.clear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(402, 113);
+            this.label1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(3, 155);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "7";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Buttonclick);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(84, 155);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "8";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Buttonclick);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(165, 155);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "9";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Buttonclick);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(246, 155);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "/";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.Buttonclick);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(3, 184);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "4";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.Buttonclick);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(84, 184);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 6;
+            this.button6.Text = "5";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.Buttonclick);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(165, 184);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(75, 23);
+            this.button7.TabIndex = 7;
+            this.button7.Text = "6";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.Buttonclick);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(246, 184);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(75, 23);
+            this.button8.TabIndex = 8;
+            this.button8.Text = "*";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.Buttonclick);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(3, 213);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(75, 23);
+            this.button9.TabIndex = 9;
+            this.button9.Text = "1";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.Buttonclick);
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(84, 213);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(75, 23);
+            this.button10.TabIndex = 10;
+            this.button10.Text = "2";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.Buttonclick);
+            // 
+            // button11
+            // 
+            this.button11.Location = new System.Drawing.Point(165, 213);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(75, 23);
+            this.button11.TabIndex = 11;
+            this.button11.Text = "3";
+            this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.Buttonclick);
+            // 
+            // button12
+            // 
+            this.button12.Location = new System.Drawing.Point(246, 213);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(75, 23);
+            this.button12.TabIndex = 12;
+            this.button12.Text = "-";
+            this.button12.UseVisualStyleBackColor = true;
+            this.button12.Click += new System.EventHandler(this.Buttonclick);
+            // 
+            // button13
+            // 
+            this.button13.Location = new System.Drawing.Point(3, 242);
+            this.button13.Name = "button13";
+            this.button13.Size = new System.Drawing.Size(156, 23);
+            this.button13.TabIndex = 13;
+            this.button13.Text = "0";
+            this.button13.UseVisualStyleBackColor = true;
+            this.button13.Click += new System.EventHandler(this.Buttonclick);
+            // 
+            // button14
+            // 
+            this.button14.Location = new System.Drawing.Point(165, 242);
+            this.button14.Name = "button14";
+            this.button14.Size = new System.Drawing.Size(75, 23);
+            this.button14.TabIndex = 14;
+            this.button14.Text = ".";
+            this.button14.UseVisualStyleBackColor = true;
+            this.button14.Click += new System.EventHandler(this.Buttonclick);
+            // 
+            // button15
+            // 
+            this.button15.Location = new System.Drawing.Point(246, 242);
+            this.button15.Name = "button15";
+            this.button15.Size = new System.Drawing.Size(75, 23);
+            this.button15.TabIndex = 15;
+            this.button15.Text = "+";
+            this.button15.UseVisualStyleBackColor = true;
+            this.button15.Click += new System.EventHandler(this.Buttonclick);
+            // 
+            // exp
+            // 
+            this.exp.Location = new System.Drawing.Point(327, 184);
+            this.exp.Name = "exp";
+            this.exp.Size = new System.Drawing.Size(75, 23);
+            this.exp.TabIndex = 16;
+            this.exp.Text = "Exponent";
+            this.exp.UseVisualStyleBackColor = true;
+            this.exp.Click += new System.EventHandler(this.Buttonclick);
+            // 
+            // sqrt
+            // 
+            this.sqrt.Location = new System.Drawing.Point(327, 213);
+            this.sqrt.Name = "sqrt";
+            this.sqrt.Size = new System.Drawing.Size(75, 23);
+            this.sqrt.TabIndex = 17;
+            this.sqrt.Text = "Square Root";
+            this.sqrt.UseVisualStyleBackColor = true;
+            this.sqrt.Click += new System.EventHandler(this.sqrt_Click);
+            // 
+            // button18
+            // 
+            this.button18.Location = new System.Drawing.Point(327, 242);
+            this.button18.Name = "button18";
+            this.button18.Size = new System.Drawing.Size(75, 23);
+            this.button18.TabIndex = 18;
+            this.button18.Text = "=";
+            this.button18.UseVisualStyleBackColor = true;
+            this.button18.Click += new System.EventHandler(this.Buttonclick);
+            // 
+            // clear
+            // 
+            this.clear.Location = new System.Drawing.Point(327, 155);
+            this.clear.Name = "clear";
+            this.clear.Size = new System.Drawing.Size(75, 23);
+            this.clear.TabIndex = 19;
+            this.clear.Text = "Clear";
+            this.clear.UseVisualStyleBackColor = true;
+            this.clear.Click += new System.EventHandler(this.clear_Click);
+            // 
+            // FrmCalc
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(407, 277);
+            this.Controls.Add(this.clear);
+            this.Controls.Add(this.button18);
+            this.Controls.Add(this.sqrt);
+            this.Controls.Add(this.exp);
+            this.Controls.Add(this.button15);
+            this.Controls.Add(this.button14);
+            this.Controls.Add(this.button13);
+            this.Controls.Add(this.button12);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.KeyPreview = true;
+            this.KeyPress += new KeyPressEventHandler(FrmCalc_KeyPress);
+            this.Name = "FrmCalc";
+            this.Text = "Calculator";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.Button button13;
+        private System.Windows.Forms.Button button14;
+        private System.Windows.Forms.Button button15;
+        private System.Windows.Forms.Button exp;
+        private System.Windows.Forms.Button sqrt;
+        private System.Windows.Forms.Button button18;
+        private System.Windows.Forms.Button clear;
+    }
+}
+
---
+++ Calculator/Calculator.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Data;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class FrmCalc : Form
+    {
+        public FrmCalc()
+        {
+            InitializeComponent();
+        }
+
+        //instead of having numerous different buttons, I'm just passing all of them that are a character of text through this
+        private void Buttonclick(object sender, EventArgs e)
+        {
+            //initilizing text string variable
+            var text ="";
+
+            //since the sender object could be a Form because of keypresses or a Button by the user clicking the button I have to check
+            if (sender is Button)
+            {
+                //we make a Button variable since we know the sender object is of the class Button
+                var btn = (Button)sender;
+                //if the length is greater than one, that means it's the exponent which we will show with the carrot symbol
+                text = btn.Text.Length > 1 ? @"^" : btn.Text;
+            }
+            else if (sender is Form)
+            {
+                //Since it's the form that means that the user is entering things via their keyboard
+                var keyevent = (KeyPressEventArgs) e;
+
+                //since keyboard inputs come in ASCII way, we need to convert them to their non ASCII values
+                text = keyevent.KeyChar.ToString();
+            }
+
+            //if label1.text is blank then no mathmatical operations should be passed
+            if (string.IsNullOrEmpty(label1.Text) && (!char.IsDigit(Convert.ToChar(text)) && text != @"."))
+                //returns just break out of the function
+                return;
+
+            //can't have back to back mathmatical operations
+            if ((!char.IsDigit(Convert.ToChar(text)) && text != @".") && label1.Text[label1.Text.Length - 1] == ' ')
+                return;
+
+            //if there's an equal sign then that means we already solved the equation so we should start from scratch
+            if (label1.Text.Contains("="))
+            {
+                //I already have a clear button, might as well use the function so I'm not making duplicate code
+                clear_Click(sender, e);
+            }
+            
+
+            //if the button that's being selected is a digit then don't put spaces around it
+            label1.Text += char.IsDigit(Convert.ToChar(text)) ? text : " " + text + " ";
+            label1.Text = label1.Text.Replace(" . ", ".");
+
+            //if there isn't an equal sign then just break out of the function
+            if (!label1.Text.Contains("=")) return;
+
+            //to use my evaluate function, there can't be an equal sign
+            label1.Text = label1.Text.Replace("= ", "").Replace(" ^ ", "^");
+            //with my all in one text writer, the periods get spaces around them, so we remove them
+
+            //^ isn't a real mathmatical operation so I can't pass it through my evaluate function
+            while (label1.Text.Contains("^"))
+            {
+                //since I know there is a ^ in my string, I need to find the index of it in the string
+                var index = label1.Text.IndexOf("^");
+
+                //since I the index of the ^, then before it should be the first number and after it will be the second number
+                //By doing it this way I'm being returned a char and since those use the ascii value, if I didn't use
+                //getnumericvalue, it would return me the ascii number instead of the decimal number
+                var first = char.GetNumericValue(label1.Text[index - 1]);
+                var second = char.GetNumericValue(label1.Text[index + 1]);
+
+                //Math.Pow is how you do exponent calculations
+                var equals = Convert.ToString(Math.Pow(first, second));
+
+                //Now I need to edit my string to add in the change so the Evaluate function can understand it
+                label1.Text = label1.Text.Replace(first + "^" + second, equals);
+            }
+
+            label1.Text = Evaluate(label1.Text);
+        }
+
+        //evaluates the mathmatical expression
+        private static string Evaluate(string expression)
+        {
+            var table = new DataTable();
+
+            //adds a column with the word "expression" and puts the first cell as the string expression
+            table.Columns.Add("expression", typeof(string), expression);
+
+            var row = table.NewRow();
+
+            //create a new row because this row will have what the expression's answer will be
+            table.Rows.Add(row);
+
+            //returns the row that would have the mathmatical expression evaluated
+            return (string) row["expression"];
+        }
+
+        private void clear_Click(object sender, EventArgs e)
+        {
+            //Since whatever is in label1.Text gets evaluated, to make it blank, we just set it to ""
+            label1.Text = "";
+        }
+
+        private void sqrt_Click(object sender, EventArgs e)
+        {
+            var text = label1.Text;
+
+            //this grabs the last character of text
+            var lastdigit = text.Substring(text.Length - 1, 1);
+
+            //if the last character isn't a digit then we blank everything out because the user tried to pass in something that wasn't possible
+            if (!char.IsDigit(Convert.ToChar(lastdigit)))
+                label1.Text = "";
+
+            else
+            {
+                //we make a new character array of text so we can reverse and then set it back to text
+                //we do this because it's possible that we're taking the square root of a number that's bigger than a single digit
+                var c = text.ToCharArray();
+                Array.Reverse(c);
+                text = new string(c);
+
+                //we find the index of the first space in the newly reverse string
+                var index = text.Contains(" ") ? text.IndexOf(" ") : text.Length;
+
+                //everything before the space that we found is our number
+                var num = text.Substring(0, index);
+
+                //now that we have our number, we need to reverse it so it's the correct way
+                c = num.ToCharArray();
+                Array.Reverse(c);
+                num = new string(c);
+
+                //we replace the number that wanted to be square rooted with the answer
+                label1.Text = label1.Text.Substring(0, label1.Text.Length - index) + Math.Sqrt(Convert.ToDouble(num));
+            }
+        }
+
+        //made keyboard shortcuts, so you can type out what you want instead of clicking buttons
+        private void FrmCalc_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            //e.KeyChar returns the ascii position of the character that was pressed
+
+            //all digits, +, -, *, ^, /, e, E
+            //enter/return key is not used because that's another way you can enter buttons so pressing E or e will do it instead
+            if ((e.KeyChar >= 45 && e.KeyChar <= 57) || e.KeyChar == 94 || e.KeyChar == 43 || e.KeyChar == 42 || e.KeyChar == 69 || e.KeyChar == 101)
+                Buttonclick(sender, e);
+            //c or C for Clear
+            else if (e.KeyChar == 67 || e.KeyChar == 99)
+                clear_Click(sender, e);
+            //s or S for Square Root
+            else if (e.KeyChar == 115 || e.KeyChar == 83)
+                sqrt_Click(sender, e);
+        }
+    }
+}
